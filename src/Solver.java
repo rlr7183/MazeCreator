ee4779a6e1049473174c62b789639d2224a3bd2c
Rachel <rlr7183>
2024-02-20 15:51:25 -0500

    my original dfs adjacent.. only finds one solution.

--- src/Solver.java
+++ src/Solver.java
@@ -2,34 +2,73 @@ import java.util.*;
 
 public class Solver {
 
-    public static List<Solution> findAllSolutions(Maze maze) {
-        Set<State> visited = new HashSet<>();
-        List<Solution> solutions = new ArrayList<>();
-        Deque<State> pathStack = new ArrayDeque<>();
+    private Maze maze;
+    private Map<State, State> parents;
+    private Set<State> visited;
+    private Deque<State> stack;
+    private List<Solution> solutions;
 
-        dfs(maze.getStart(), visited, pathStack, solutions, maze);
-
-        return solutions;
+    public Solver(Maze maze) {
+        this.maze = maze;
+        this.parents = new HashMap<>();
+        this.visited = new HashSet<>();
+        this.stack = new ArrayDeque<>();
+        this.solutions = new ArrayList<>();
     }
 
-    private static void dfs(State current, Set<State> visited, Deque<State> pathStack, List<Solution> solutions, Maze maze) {
-        visited.add(current);
-        pathStack.push(current);
+    public List<Solution> dfs() {
+        State start = maze.getStart();
+        stack.push(start);
+        visited.add(start);
 
-        if (current.equals(maze.getGoal())) {
-            solutions.add(new Solution(new ArrayList<>(pathStack)));
-            if(solutions.size()>10){
-                return;
+        while (!stack.isEmpty()) {
+            State current = stack.pop();
+            if (current.equals(maze.getGoal())) {
+                Solution possibleSol = makePath(current);
+                //addSolution(possibleSol);
+                solutions.add(possibleSol);
+                return solutions;
             }
-        } else {
-            for (State next : maze.legalNeighbors(current)) {
-                if (!visited.contains(next)) {
-                    dfs(next, visited, pathStack, solutions, maze);
+            List<State> neighbors = maze.legalNeighbors(current);
+            for (State n : neighbors) {
+                if (!visited.contains(n)) {
+                    parents.put(n, current);
+                    stack.push(n);
+                    visited.add(n);
                 }
             }
         }
 
-        //visited.remove(current);
-        pathStack.pop();
+        return solutions;
     }
+
+    private Solution makePath(State goal) {
+        List<State> path = new ArrayList<>();
+        State state = goal;
+        while (state != null) {
+            path.add(state);
+            state = parents.get(state);
+        }
+        Collections.reverse(path);
+
+        return new Solution(path);
+    }
+
+    private void addSolution(Solution currentSolution) {
+        for (Solution s : solutions) {
+            if (s.equals(currentSolution)) {
+                return;
+            }
+        }
+        if(!currentSolution.meetReq(DifficultyLevel.EASY)) {
+            return;
+        }
+        Set<State> setComp = new HashSet<>(currentSolution.getSol());
+        if (setComp.size() != currentSolution.getSolLen()) {
+            return;
+        }
+        solutions.add(currentSolution);
+    }
+
+
 }
